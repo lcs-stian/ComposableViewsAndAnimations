bce3cf593f970d1a626425fa282f06522bf914a4
Suzanne <STian@lcs.on.ca>
2022-04-26 23:49:54 -0400

    Star

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -9,29 +9,77 @@ import SwiftUI
 
 struct CustomComposableView: View {
     //MARK: Stored properties
-    //control the horizontal offset
-    @State var offset = 0.0
-    //MARK: Computed properties
     
+    // Whether to apply the animation
+    @State private var useAnimation = true
+    
+    // Controls the size of the circle
+    @State private var scaleFactor: CGFloat = 5.0
+    
+    //Control tehretation angle
+    @State var currentRotationAngle = Angle.degrees(120)
+
+
+    // Opacity of each part of the view
+    @State private var opacityWhite = 1.0
+    @State private var opacityYellow = 0.0
+    @State private var opacityOuter1 = 0.0
+    @State private var opacityOuter2 = 0.0
+    
+    
+    let timer = Timer.publish(every: 1.00, on: .main, in: .common).autoconnect()
+    
+    //MARK: Computed properties
     var body: some View {
+        
+        
         ZStack{
             
-            Circle()
-                .frame(width: 50, height: 50)
-                
+            Image(systemName: "star.fill")
+                .foregroundColor(Color(.systemGray))
+                .opacity(opacityWhite)
+                .scaleEffect(scaleFactor)
+           
             
-            Text("OK")
-                .foregroundColor(.white)
+            Image(systemName: "sun.max.fill")
+                .foregroundColor(Color(.systemYellow))
+                .opacity(opacityOuter1)
+                .scaleEffect(scaleFactor)
+                .rotationEffect(currentRotationAngle, anchor: .center)
+
+
+            
+            Image(systemName: "star.fill")
+                .foregroundColor(Color(.systemYellow))
+                .opacity(opacityYellow)
+                .scaleEffect(scaleFactor)
+        
+            
+            
+                .onReceive(timer) { input in
+                    
+        
+                    withAnimation(Animation.easeIn(duration: 0.5).delay(0.0)) {
+                        opacityWhite = 0.0
+                    }
+                    
+                    withAnimation(Animation.easeIn(duration: 0.5).delay(0.25)) {
+                        opacityYellow = 1.0
+                    }
+                    
+                    withAnimation(Animation.easeIn(duration: 0.5).delay(0.5)) {
+                        opacityOuter1 = 1.0
+                    }
+                    
+                    // Stop the timer
+                    timer.upstream.connect().cancel()
+                    
+                }
+            
         }
-       .offset(x: offset, y: 0)
-        onTapGesture {
-            //move to the right
-            offset = 100.0
-        }
-        .animation(
-            Animation.easeInOut(duration: 2.0)
-        )
-      
+        
+        
+        
     }
 }
 
