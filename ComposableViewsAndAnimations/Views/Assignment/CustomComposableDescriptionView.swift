ccc423b3b10595ae55c7bcc0ed2c260d120b23c2
Suzanne <STian@lcs.on.ca>
2022-04-30 22:55:46 -0400

    Finish Star animation with blurAmount slider.

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableDescriptionView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableDescriptionView.swift
@@ -12,6 +12,8 @@ struct CustomComposableDescriptionView: View {
     // MARK: Stored properties
     @State private var phrase: String = ""
     
+    @State private var blurAmount: CGFloat = 0.0
+    
     // MARK: Computed properties
     var body: some View {
         
@@ -25,7 +27,7 @@ struct CustomComposableDescriptionView: View {
                     .padding(.top)
                 
                 Text("""
-                    My Composable View 1 is a star, this could be used to mark thing important in your app
+                    My Composable View 1 is a star, this could be used to mark thing important in your app or to test vocabularies using blur.
                                        
                                        
                     My Composable View 2 is a heart with a broader that has a 3D effect, this could be used to like something.
@@ -34,11 +36,16 @@ struct CustomComposableDescriptionView: View {
                 
                 TextField("Enter an input value", text: $phrase)
                 
+                Slider(value: $blurAmount, in: 0...20, step: 1.0) {
+                    Text("Blur amount")
+                }
+                
             }
+            .padding()
             .padding(.bottom)
             
             List {
-                NavigationLink(destination: CustomComposableView(message: phrase)) {
+                NavigationLink(destination: CustomComposableView(message: phrase, blurAmount: blurAmount)) {
                     SimpleListItemView(title: "My Composable View 1",
                                        caption: "The animation will have a star and say: \(phrase)")
                 }
--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -4,78 +4,88 @@
 //
 //  Created by Russell Gordon on 2021-02-24.
 //
- 
+
 import SwiftUI
- 
+
 struct CustomComposableView: View {
     //MARK: Stored properties
     
     // What to say?
     let message: String
     
+    let blurAmount: CGFloat
+    
+   
+   
     // Whether to apply the animation
     @State private var useAnimation = true
     
     // Controls the size
-    @State private var scaleFactor: CGFloat = 5.0
+    @State private var scaleFactor: CGFloat = 2.0
+   
     
-    //control offset
-    @State var xOffset = 50.0
-    
-
     // Opacity of each part of the view
-    @State private var opacityWhite = 1.0
-    @State private var opacityYellow = 0.0
+    @State private var opacity1 = 1.0
+    @State private var opacity2 = 0.0
     
-
+    //change the scale effect value
+    @State private var animationAmount = 1.0
+    
+  
     
     //MARK: Computed properties
     var body: some View {
         
         
-        ZStack{
+        HStack (spacing:20){
             
-     
-            
-            Image(systemName: "star.fill")
-                .foregroundColor(Color(.systemGray))
-                .opacity(opacityWhite)
-                .scaleEffect(scaleFactor)
-            
-            Image(systemName: "star.fill")
-                .foregroundColor(Color(.systemYellow))
-                .opacity(opacityYellow)
-                .scaleEffect(scaleFactor)
+            ZStack{
+                
+                Image(systemName: "star.fill")
+                    .foregroundColor(Color(.systemYellow))
+                    .opacity(opacity2)
+                    .scaleEffect(scaleFactor)
+                    
+                    
+                
+                Image(systemName: "star.fill")
+                    .foregroundColor(Color(.systemGray))
+                    .opacity(opacity1)
+                    .scaleEffect(scaleFactor)
+                
+            }
             
             Text("\(message)")
                 .font(Font.custom("Chalkduster", size: 18.0, relativeTo: .largeTitle))
-                .offset(x: xOffset, y: 0)
-                .opacity(opacityYellow)
+                .opacity(opacity2)
+                .blur(radius: blurAmount)
+                
+        }
+        
+        
+        
+        .onTapGesture {
+            print ("inside")
             
-            onTapGesture {
-                
-                withAnimation(Animation.easeIn(duration: 0.5).delay(0.0)) {
-                    opacityWhite = 0.0
-                }
-                
-                withAnimation(Animation.easeIn(duration: 0.5).delay(0.25)) {
-                    opacityYellow = 1.0
-                }
-                
+            withAnimation(Animation.easeIn(duration: 0.5).delay(0.0)) {
+                opacity1 = 0.0
             }
-                    
-                  
-                    
-                }
+            
+            withAnimation(Animation.easeIn(duration: 0.5).delay(0.25)) {
+                opacity2 = 1.0
+            }
             
         }
         
         
     }
+    
+    
+}
 
- 
+
 struct CustomComposableView_Previews: PreviewProvider {
     static var previews: some View {
-        CustomComposableView(message: "massage")
+        CustomComposableView(message: "massage", blurAmount: 20.0)
     }
 }
